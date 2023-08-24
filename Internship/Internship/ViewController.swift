4f32afbdaeb5da2f2796a436a698adf10fa506ea
Ryabova Veronika IU5-55B <70639339+nika-rvv@users.noreply.github.com>
2023-08-24 20:34:57 +0300

    add layout of cell & collection view

--- Internship/Internship/ViewController.swift
+++ Internship/Internship/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let itemsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(ItemsCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return cv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
+        setupCollectionView()
     }
-
-
 }
 
+private extension ViewController {
+    func setupViews() {
+        view.addSubview(itemsCollectionView)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            itemsCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 16),
+            itemsCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            itemsCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            itemsCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    func setupCollectionView() {
+        itemsCollectionView.delegate = self
+        itemsCollectionView.dataSource = self
+        itemsCollectionView.showsVerticalScrollIndicator = false
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ItemsCollectionViewCell
+        cell.backgroundColor = .red
+        cell.configureCellWith(imageName: "Image", title: "Iphone", price: "55000", location: "Moscow", date: "23/08/23")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width / 2.35, height: view.frame.height / 3.47)
+    }
+}
---
+++ Internship/Internship/Views/ItemsCollectionViewCell.swift
@@ -0,0 +1,123 @@
+//
+//  ItemsCollectionViewCell.swift
+//  Internship
+//
+//  Created by Veronika on 24.08.2023.
+//
+
+import UIKit
+
+final class ItemsCollectionViewCell: UICollectionViewCell {
+//    private let itemStackView: UIStackView = {
+//        let stackView = UIStackView()
+//        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        stackView.axis = .vertical
+//        stackView.spacing = 4
+////        stackView.distribution = .fill
+//        return stackView
+//    }()
+    
+    private let itemImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.layer.cornerRadius = 4
+        image.clipsToBounds = true
+        image.contentMode = .scaleToFill
+        return image
+    }()
+    
+    private let itemTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.numberOfLines = 2
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        return label
+    }()
+    
+    private let itemPriceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        return label
+    }()
+    
+    private let itemLocationLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        return label
+    }()
+    
+    private let itemCreationDateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        return nil
+    }
+}
+
+private extension ItemsCollectionViewCell {
+    func setupViews() {
+        self.addSubview(itemImage)
+        self.addSubview(itemTitleLabel)
+        self.addSubview(itemPriceLabel)
+        self.addSubview(itemLocationLabel)
+        self.addSubview(itemCreationDateLabel)
+//
+//        itemStackView.addArrangedSubview(itemImage)
+//        itemStackView.addArrangedSubview(itemTitleLabel)
+//        itemStackView.addArrangedSubview(itemPriceLabel)
+//        itemStackView.addArrangedSubview(itemLocationLabel)
+//        itemStackView.addArrangedSubview(itemCreationDateLabel)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            itemImage.topAnchor.constraint(equalTo: self.topAnchor),
+            itemImage.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            itemImage.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            itemImage.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.67),
+            
+            itemTitleLabel.topAnchor.constraint(equalTo: itemImage.bottomAnchor, constant: 4),
+            itemTitleLabel.leadingAnchor.constraint(equalTo: itemImage.leadingAnchor),
+            itemTitleLabel.trailingAnchor.constraint(equalTo: itemImage.trailingAnchor),
+            
+            itemPriceLabel.topAnchor.constraint(equalTo: itemTitleLabel.bottomAnchor, constant: 4),
+            itemPriceLabel.leadingAnchor.constraint(equalTo: itemImage.leadingAnchor),
+            itemPriceLabel.trailingAnchor.constraint(equalTo: itemImage.trailingAnchor),
+            
+            itemLocationLabel.topAnchor.constraint(equalTo: itemPriceLabel.bottomAnchor, constant: 4),
+            itemLocationLabel.leadingAnchor.constraint(equalTo: itemImage.leadingAnchor),
+            itemLocationLabel.trailingAnchor.constraint(equalTo: itemImage.trailingAnchor),
+            
+            itemCreationDateLabel.topAnchor.constraint(equalTo: itemLocationLabel.bottomAnchor, constant: 4),
+            itemCreationDateLabel.leadingAnchor.constraint(equalTo: itemImage.leadingAnchor),
+            itemCreationDateLabel.trailingAnchor.constraint(equalTo: itemImage.trailingAnchor),
+            
+        ])
+    }
+}
+
+extension ItemsCollectionViewCell {
+    func configureCellWith(imageName: String, title: String, price: String, location: String, date: String) {
+        itemImage.image = UIImage(named: imageName)
+        itemTitleLabel.text = title
+        itemPriceLabel.text = price
+        itemLocationLabel.text = location
+        itemCreationDateLabel.text = date
+    }
+}
