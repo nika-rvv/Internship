796f2042a0e7b8828cbd695f6f2d2c51713e4248
Ryabova Veronika IU5-55B <70639339+nika-rvv@users.noreply.github.com>
2023-08-24 17:37:55 +0300

    Initial project

---
+++ Internship/Internship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Internship
+//
+//  Created by Veronika on 24.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
